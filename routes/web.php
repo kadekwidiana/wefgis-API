dfa48eb50b83a793011a9f7956c4262c9fe96a29
kadekwidiana <kdkonos10@gmail.com>
2023-12-29 20:48:14 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -1,46 +1,50 @@
 <?php
 
-use App\Http\Controllers\CropChacoengsaoController;
-use App\Http\Controllers\NakhonController;
-use App\Http\Controllers\ProfileController;
+// use App\Http\Controllers\CropChacoengsaoController;
+// use App\Http\Controllers\NakhonController;
+// use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// /*
+// |--------------------------------------------------------------------------
+// | Web Routes
+// |--------------------------------------------------------------------------
+// |
+// | Here is where you can register web routes for your application. These
+// | routes are loaded by the RouteServiceProvider and all of them will
+// | be assigned to the "web" middleware group. Make something great!
+// |
+// */
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
+
+// require __DIR__ . '/auth.php';
+
+// // view frontpage
+// Route::get('/', [CropChacoengsaoController::class, 'index'])->name('index');
+
+// Route::get('/pointCrop', [CropChacoengsaoController::class, 'pointCrop'])->name('json-crop');
+// // GEE
+// Route::get('/wateroccurence', [CropChacoengsaoController::class, 'waterOccurrence'])->name('waterOccurrence');
+// Route::post('/precipitation',[CropChacoengsaoController::class, 'precipitation'])->name('precipitation');
+// Route::post('/vci',[CropChacoengsaoController::class, 'vci'])->name('vci');
+// Route::post('/evi',[CropChacoengsaoController::class, 'evi'])->name('evi');
+// // phenology crop
+// Route::post('/phenology_crop',[CropChacoengsaoController::class, 'phenology_crop'])->name('phenology_crop');
+
+// // GEE Nakhon Pathom
+// Route::get('/nakhonwater', [CropChacoengsaoController::class, 'nakhonWater'])->name('nakhonWater');
+// Route::get('/nakhonmap', [CropChacoengsaoController::class, 'nakhonMap'])->name('nakhonMap');
+// Route::get('/pointNakhon', [NakhonController::class, 'pointNakhon'])->name('point-nakhon');
+
+Route::get('/', function () {
+    return view('welcome');
 });
-
-require __DIR__ . '/auth.php';
-
-// view frontpage
-Route::get('/', [CropChacoengsaoController::class, 'index'])->name('index');
-
-Route::get('/pointCrop', [CropChacoengsaoController::class, 'pointCrop'])->name('json-crop');
-// GEE
-Route::get('/wateroccurence', [CropChacoengsaoController::class, 'waterOccurrence'])->name('waterOccurrence');
-Route::post('/precipitation',[CropChacoengsaoController::class, 'precipitation'])->name('precipitation');
-Route::post('/vci',[CropChacoengsaoController::class, 'vci'])->name('vci');
-Route::post('/evi',[CropChacoengsaoController::class, 'evi'])->name('evi');
-// phenology crop
-Route::post('/phenology_crop',[CropChacoengsaoController::class, 'phenology_crop'])->name('phenology_crop');
-
-// GEE Nakhon Pathom
-Route::get('/nakhonwater', [CropChacoengsaoController::class, 'nakhonWater'])->name('nakhonWater');
-Route::get('/nakhonmap', [CropChacoengsaoController::class, 'nakhonMap'])->name('nakhonMap');
-Route::get('/pointNakhon', [NakhonController::class, 'pointNakhon'])->name('point-nakhon');
